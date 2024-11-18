75a8453df25b25ba34f4a575c0c4a1d6c13fb3f1
David Zajicek <david.a.zajicek@outlook.com>
2024-11-18 15:01:54 +1100

    1

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,12 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+  static initializeRooms() {
+    const array = [];
+    array[0] = new ClassRoom(19);
+    array[1] = new ClassRoom(20);
+    array[2] = new ClassRoom(34);
+    return array;
+  }
+}
