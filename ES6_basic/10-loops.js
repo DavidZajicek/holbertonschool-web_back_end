2139125f1ea7d3f071c2eeb56b9b69ec3cbec886
David Zajicek <david.a.zajicek@outlook.com>
2024-11-18 13:05:38 +1100

    100

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  let index = 0;
   for (let s of array) {
-    s = appendString + s;
+    newArray[index] = appendString.concat(s);
+    index += 1;
   }
 
-  return array;
+  return newArray;
 }
