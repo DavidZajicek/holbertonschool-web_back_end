d99add84aba287a146e79d0b62075fd56f274793
David Zajicek <david.a.zajicek@outlook.com>
2024-11-18 15:36:14 +1100

    get

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -2,6 +2,9 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     (this.name = name), (this.length = length), (this.students = students);
   }
+  get name() {
+    return this._name;
+  }
   set name(name) {
     if (typeof name === 'string') {
       this._name = name;
@@ -9,12 +12,18 @@ export default class HolbertonCourse {
       throw new TypeError('Name must be a string');
     }
   }
+  get length() {
+    return this._length;
+  }
   set length(length) {
     if (typeof length === 'number') {
       this._length = length;
     } else {
       throw new TypeError('Length must be a number');
     }
+  }
+  get students() {
+    return this._students;
   }
   set students(students) {
     if (Array.isArray(students)) {
