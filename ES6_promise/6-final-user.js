5dfa60f45af973f75413e0376cc0659463749456
David Zajicek <david.a.zajicek@outlook.com>
2024-11-20 10:42:57 +1100

    shrug

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,9 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, filename) {
-  const results = [];
+  const userReponse = await signUpUser(firstName, lastName);
+  const photoReponse = await uploadPhoto(filename).catch((reject) => reject);
 
-  results.push(await signUpUser(firstName, lastName));
-  results.push(await uploadPhoto(filename).catch((reject) => reject));
-  return results;
+  return [userReponse, photoReponse];
 }
