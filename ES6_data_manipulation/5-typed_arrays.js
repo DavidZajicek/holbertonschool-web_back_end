de2f47603a207b767bca310d413f701e3aaf9b58
David Zajicek <david.a.zajicek@outlook.com>
2024-11-19 20:10:29 +1100

    So.. you wanted a DataView? Be more explicit please

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
   try {
     const arrayBuffer = new ArrayBuffer(length);
-    const array = new Int8Array(arrayBuffer);
-    array.set([value], position);
+    const array = new DataView(arrayBuffer);
+    array.setInt8(position, value);
     return array;
   } catch (error) {
     throw new Error('Position outside range');
