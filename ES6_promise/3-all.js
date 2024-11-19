cef52436beafbdc601f9ab9eb453207b3347aa61
David Zajicek <david.a.zajicek@outlook.com>
2024-11-20 09:54:42 +1100

    catch

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,10 +2,14 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
   const results = [];
-  await uploadPhoto().then((result) => results.push(result.body));
-  await createUser().then((result) => {
-    results.push(result.firstName);
-    results.push(result.lastName);
-  });
+  await uploadPhoto()
+    .then((result) => results.push(result.body))
+    .catch(() => console.log('Signup system offline'));
+  await createUser()
+    .then((result) => {
+      results.push(result.firstName);
+      results.push(result.lastName);
+    })
+    .catch(() => console.log('Signup system offline'));
   console.log(results.join(' '));
 }
