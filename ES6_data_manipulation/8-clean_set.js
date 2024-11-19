fc7a41a3a137b0f50a8ec885865568d33e8cd9d9
David Zajicek <david.a.zajicek@outlook.com>
2024-11-19 20:58:03 +1100

    undefined

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,10 @@
 export default function cleanSet(set, startString) {
   const matching = [];
+  if (typeof startString !== 'string' || startString === '') {
+    return '';
+  }
   set.forEach((value) => {
-    if (value.startsWith(startString) && startString !== '') {
+    if (value.startsWith(startString)) {
       matching.push(value.slice(startString.length));
     }
   });
