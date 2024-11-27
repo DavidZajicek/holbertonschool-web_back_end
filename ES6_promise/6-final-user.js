563da8e00f4baaf32d5eb9b6317c8bf134014c69
David Zajicek <david.a.zajicek@outlook.com>
2024-11-27 11:59:53 +1100

    not settled?

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,7 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, filename) {
   return Promise.all([
-    signUpUser(firstName, lastName),
+    signUpUser(firstName, lastName).then((value) => ({
+      status: 'fulfilled',
+      value: value,
+    })),
     uploadPhoto(filename).catch((e) => ({
       status: 'rejected',
       value: e.toString(),
