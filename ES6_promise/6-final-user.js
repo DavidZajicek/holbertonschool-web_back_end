ba9b4717058e4d3fed4c2624840dbcabb7355ee8
David Zajicek <david.a.zajicek@outlook.com>
2024-11-27 11:56:09 +1100

    this hurts me deeply

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,5 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]);
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(filename).catch((e) => ({
+      status: 'rejected',
+      value: e.toString(),
+    })),
+  ]);
 }
