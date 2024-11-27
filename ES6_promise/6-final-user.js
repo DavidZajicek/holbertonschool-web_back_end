27a851c50809e69a3fb02a5d354f01bbde2f57ba
David Zajicek <david.a.zajicek@outlook.com>
2024-11-27 11:58:31 +1100

    all?

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([
+  return Promise.all([
     signUpUser(firstName, lastName),
     uploadPhoto(filename).catch((e) => ({
       status: 'rejected',
