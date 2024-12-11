89c177edc56dcbb04a2d32680e2d493940b638a4
David Zajicek <david.a.zajicek@outlook.com>
2024-12-11 11:43:50 +1100

    6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+docstring
+"""
+
+
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[float, int]) -> float:
+    """ docstring """
+    return sum(mxd_lst)
