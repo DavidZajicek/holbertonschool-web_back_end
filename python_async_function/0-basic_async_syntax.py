8d528412894ff856d458e3a3ddd514cfb46bf95b
David Zajicek <david.a.zajicek@outlook.com>
2024-12-11 12:12:34 +1100

    That's on you for not letting me only import sleep

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -3,12 +3,12 @@
 docstring
 """
 
-from asyncio import sleep
+import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """ docstring """
     delay = random.uniform(0, max_delay)
-    await sleep(delay)
+    await asyncio.sleep(delay)
     return delay
