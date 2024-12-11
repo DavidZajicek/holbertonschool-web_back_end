ad897ff98de58edb362b5958a3b7968c6c25b4d5
David Zajicek <david.a.zajicek@outlook.com>
2024-12-11 13:58:31 +1100

    1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+docstring
+"""
+
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int = 0, max_delay: int = 10):
+    """ docstring """
+    loop = asyncio.get_event_loop()
+    tasks = []
+    for _ in range(n):
+        tasks.append(loop.create_task(wait_random(max_delay)))
+    results = await asyncio.gather(*tasks)
+    return results
