9fccfc5f9b065bdd121b99ebb072a1ccf0736e39
David Zajicek <david.a.zajicek@outlook.com>
2024-12-11 15:15:26 +1100

    none none

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """doctstring"""
     for _ in range(10):
         yield random.uniform(0, 10)
