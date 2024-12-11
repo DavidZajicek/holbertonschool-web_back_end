3b6a7322e1a7e1162751aa116717d45f47426f49
David Zajicek <david.a.zajicek@outlook.com>
2024-12-11 13:59:53 +1100

    typing

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,12 +4,13 @@ docstring
 """
 
 import asyncio
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int = 0, max_delay: int = 10):
+async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
     """ docstring """
     loop = asyncio.get_event_loop()
     tasks = []
